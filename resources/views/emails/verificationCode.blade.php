2c93b95afb4c8246c51307311d710d89420a92d3
Martin Calderon <elmcwl@gmail.com>
2023-12-15 18:06:37 -0700

    feat: added mail delivery functionality

---
+++ resources/views/emails/verificationCode.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Correo electrónico</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis esse nobis, impedit recusandae eos temporibus sapiente fuga possimus obcaecati voluptatum harum, suscipit corrupti officiis doloribus, dignissimos similique cupiditate eius? Ea!</p>
+</body>
+</html>
