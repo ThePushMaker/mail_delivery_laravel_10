1dced8504e3b2d7ab4bebb3157f43ae01181626a
Martin Calderon <elmcwl@gmail.com>
2023-12-19 05:13:38 -0700

    feat added breeze and verification email part 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Mail\VerificationMailable;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('verification', function () {
-    Mail::to('destination@mail.com')
-        ->send(new VerificationMailable);
-    return "Mensaje enviado";
-})->name('verificationCode');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
